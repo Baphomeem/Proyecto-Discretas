f2f2b6fcece8134da5d556d56ab2187959b010a2
Baphomeem <116759900+Baphomeem@users.noreply.github.com>
2025-07-22 19:56:00 -0500

    main.py

--- main.py
+++ main.py
@@ -1,3 +1,155 @@
+Saskiel
+saskiel_
+Invisible
+
+Saskiel — 20/07/2025 16:40
+m!p rara bien
+Jockie Music (1)
+APP
+ — 20/07/2025 16:40
+Did you know you now get better audio quality with any premium tier https://www.patreon.com/Jockie!
+:deezer: ​ Started playing Rara Bien by Rupatrupa
+Saskiel — 20/07/2025 16:44
+Expansión de Expresiones Algebraicas Usando Python y LaTeX
+Jockie Music (1)
+APP
+ — 20/07/2025 16:46
+There are no more tracks
+No tracks have been playing for the past 3 minutes, leaving :wave:
+
+This can be disabled by using the premium command 24/7, for more information check out the perks command!
+IsabelSepulveda — 20/07/2025 17:18
+Tipo de archivo adjunto: document
+Proyecto_discretas.docx
+29.84 KB
+Saskiel — 20/07/2025 17:23
+Tipo de archivo adjunto: document
+Informe - P4 (1).docx
+38.45 KB
+Saskiel — ayer a las 21:11
+m!p colmillo tainy
+Jockie Music (1)
+APP
+ — ayer a las 21:11
+:deezer: ​ Started playing COLMILLO by Tainy
+There are no more tracks
+:spotify: ​ Started playing Bien Loco by Nova y Jory
+Jockie Music (1)
+APP
+ — ayer a las 21:19
+:spotify: ​ Started playing Pa' Que La Pases Bien by Arcángel
+Did you know you now get better audio quality with any premium tier https://www.patreon.com/Jockie!
+:spotify: ​ Started playing Mírala Bien by Wisin & Yandel
+:spotify: ​ Started playing Dile a Tu Amiga by Dalmata
+Jockie Music (1)
+APP
+ — ayer a las 21:29
+:spotify: ​ Started playing Regalame Una Noche by J Alvarez
+:spotify: ​ Started playing Rakata by Wisin & Yandel
+Jockie Music (1)
+APP
+ — ayer a las 21:36
+:spotify: ​ Started playing Hay Algo en Ti by Luny Tunes
+:spotify: ​ Started playing Cazador (feat. Ñengo Flow) by Nova y Jory
+Saskiel — ayer a las 21:42
+Imagen
+Jockie Music (1)
+APP
+ — ayer a las 21:44
+:spotify: ​ Started playing Candy by Plan B
+IsabelSepulveda — ayer a las 21:47
+#Importe de la libreria tkinter para implementacion de la interfaz
+import tkinter as tk
+
+#Toma la entrada y reescribe los simbolos ( ) {} $ +- y -
+def limpiar_entrada(f):
+    f = f.replace(" ", "").replace("-", "+-")
+Expandir
+Polinomios_interfaz.py
+6 KB
+Jockie Music (1)
+APP
+ — ayer a las 21:48
+:spotify: ​ Started playing Gata Fiera (feat. Joan) by Trebol Clan
+:spotify: ​ Started playing Punto y Aparte by Tego Calderón
+:spotify: ​ Started playing Dale Don Dale by Don Omar
+Jockie Music (1)
+APP
+ — ayer a las 21:58
+:spotify: ​ Started playing Nadie Como Tu by Wisin & Yandel
+esmo — ayer a las 22:00
+m!s
+Jockie Music (1)
+APP
+ — ayer a las 22:00
+Skipping, 1/4 (3 votes required)
+esmo — ayer a las 22:00
+tf
+Jockie Music (1)
+APP
+ — ayer a las 22:02
+:spotify: ​ Started playing Mi Noche by Reykon
+:spotify: ​ Started playing Guatauba by Plan B
+:spotify: ​ Started playing Te Imagino by Alberto Stylee
+Jockie Music (1)
+APP
+ — ayer a las 22:12
+:spotify: ​ Started playing No Es Culpa Mía by Daddy Yankee
+:spotify: ​ Started playing Te la Tiro Pa Que Bailes by Dj Joe
+Saskiel — ayer a las 22:15
+m!skip
+Jockie Music (1)
+APP
+ — ayer a las 22:15
+Te la Tiro Pa Que Bailes by Dj Joe has been skipped by @Saskiel (requester privilege)
+Jockie Music (1)
+APP
+ — ayer a las 22:15
+:spotify: ​ Started playing Sólo Pasajero by Luigi 21 Plus
+:spotify: ​ Started playing Cuando Me Dirá by Ñengo Flow
+:spotify: ​ Started playing Acechándote by Yaga & Mackie
+Jockie Music (1)
+APP
+ — ayer a las 22:25
+:spotify: ​ Started playing Shorty by Casa De Leones
+:spotify: ​ Started playing Cositas Que Haciamos (feat. Farruko) by Musicologo Y Menes
+Jockie Music (1)
+APP
+ — ayer a las 22:33
+:spotify: ​ Started playing Señal De Vida by Ñejo & Dalmata
+Xephblade — ayer a las 22:34
+@saskiel
+@Saskiel
+juanes
+venga q es jodiendo
+Jockie Music (1)
+APP
+ — ayer a las 22:37
+:spotify: ​ Started playing Matador - Official Remix by Nova y Jory
+:spotify: ​ Started playing Escápate Conmigo - Remix by Wolfine
+Jockie Music (1)
+APP
+ — ayer a las 22:44
+:spotify: ​ Started playing Gata Oficial by Luigi 21 Plus
+:spotify: ​ Started playing Salió El Sol by Don Omar
+No one has been listening for the past 3 minutes, leaving :wave:
+
+This can be disabled by using the premium command 24/7, for more information check out the perks command!
+IsabelSepulveda — 19:55
+#Importe de la libreria tkinter para implementacion de la interfaz
+import tkinter as tk
+
+#Toma la entrada y reescribe los simbolos ( ) {} $ +- y -
+def limpiar_entrada(f):
+    while ")^{" in f:
+Expandir
+Main.py
+7 KB
+﻿
+#Importe de la libreria tkinter para implementacion de la interfaz
+import tkinter as tk
+
+#Toma la entrada y reescribe los simbolos ( ) {} $ +- y -
 def limpiar_entrada(f):
     while ")^{" in f:
         pos_f = f.find(")^{")
@@ -13,6 +165,7 @@ def limpiar_entrada(f):
     f = f.replace("(", " ").replace(")", " ")
     return f.strip().split("  ")
 
+# Separa los factores en cada termino, devolviendo cada factor en un arreglo de 2 elementos (cada termino)
 def separar_terminos(factores):
     coeficientes = []
     for factor in factores:
@@ -20,6 +173,7 @@ def separar_terminos(factores):
         coeficientes.append([t for t in terminos if t])
     return coeficientes
 
+#Toma los terminos y determina los coeficientes y exponentes
 def extraer_coeficientes_exponentes(terminos):
     coeficientes = []
     exponentes = []
@@ -44,6 +198,7 @@ def extraer_coeficientes_exponentes(terminos):
         exponentes.append(e_factor)
     return coeficientes, exponentes
 
+#determina el valor de lso terminos que componen el polinomio y sus exponentes
 def multiplicar_polinomios(coeficientes, exponentes):
     while len(coeficientes) >= 2:
         a, b = coeficientes[0], coeficientes[1]
@@ -54,6 +209,7 @@ def multiplicar_polinomios(coeficientes, exponentes):
         exponentes = [sumaE] + exponentes[2:]
     return coeficientes[0], exponentes[0]
 
+#suma terminos cuyo grado sea el mismo
 def sumar_terminos_semejantes(coeficientes, exponentes):
     i = 0
     while i < len(exponentes):
@@ -73,6 +229,7 @@ def sumar_terminos_semejantes(coeficientes, exponentes):
         i += 1
     return coeficientes, exponentes
 
+#ordena los terminos de mayor a menor grado
 def ordenar(coeficientes, exponentes):
     pares = sorted(zip(exponentes, coeficientes), reverse=True)
     exp_ordenado, coef_ordenado = zip(*pares)
@@ -86,12 +243,14 @@ def formato_LaTex(coef, exp):
             termino += str(c)
         if e != 0:
             termino += 'x'
-        if e > 1 or e < 0 :
+        if e > 1:
             termino += f'^{{{e}}}'
         pol.append(termino)
     return '$' + ' + '.join(pol).replace(' + -', ' - ') + '$'
 
-def procesar_entrada(f):
+#toma la entrada ingresada por el usuario y determina factores terminos y coeficientes con las otras funciones
+def procesar_entrada():
+    f = polinomio.get()
     factores = limpiar_entrada(f)
     terminos = separar_terminos(factores)
     coeficientes, exponentes = extraer_coeficientes_exponentes(terminos)
@@ -100,13 +259,19 @@ def procesar_entrada(f):
     coef_final, exp_final = ordenar(coef_final, exp_final)
     return formato_LaTex(coef_final, exp_final)
 
-# Programa principal
-if __name__ == "__main__":
-    entrada = input()
-    resultado = procesar_entrada(entrada)
-    print(resultado)
+#ejecuta la funcion procesar entrada para obtener el resultado 
+def ejecutar():
+    procesado = procesar_entrada() 
+    salida_resultado.config(state='normal')  # Permite editar temporalmente
+    salida_resultado.delete("1.0", tk.END)
+    salida_resultado.insert(tk.END, procesado)
+    salida_resultado.config(state='disabled')  # Bloquea edición después
 
-
+def copiar_resultado():
+    texto = salida_resultado.get("1.0", tk.END).strip()
+    wind.clipboard_clear()
+    wind.clipboard_append(texto)
+    
 #IMPLEMENTACION DE LA INTERFAZ GRAFICA
 
 #Creacion de la ventana principal y especificaciones 
@@ -132,14 +297,16 @@ tk.Label(
     frame, 
     text="Ingrese el polinomio en LaTeX (Ej: $(x+1)(x+2)$):",
     bg="#f0f0f0",
-    font=('arial', 12)
+    font=('times new roman', 18)
     
     ).pack(pady=10)
 
 #creacion de entrada de texto
 polinomio=tk.Entry(
     frame,
-    width=30)
+    width=40,
+    font=('times new roman',14)
+    )
 
 polinomio.pack(pady=10)
 
@@ -147,22 +314,33 @@ polinomio.pack(pady=10)
 tk.Button(
     frame,
     text='Ejecutar',
-    font=('arial', 12),
+    font=('times new roman', 12),
     bg="#81a3b0",
     #command= llamar funcion como objeto
     command=ejecutar).pack(pady=10)
 
 resultado = tk.StringVar()
 
+
 #Etiqueta de outpout del resultado
-tk.Label(
-    frame, 
-    textvariable=resultado, 
-    font=("arial", 14), 
-    bg="#f0f0f0"
-    ).pack(pady=10)
+salida_resultado = tk.Text(
+    frame,
+    font=("times new roman", 14),
+    bg="#ffffff",
+    height=1,
+    width=40,
+    wrap='word'
+)
+salida_resultado.pack(pady=10)
+
+
+tk.Button(
+    frame,
+    text='Copiar',
+    font=('times new roman', 12),
+    bg="#81a3b0",
+    command=copiar_resultado
+).pack(pady=5)
+
 
 wind.mainloop()
-
-
- 
